6d85d6a128183aeda522bea05fea3f78e979be19
shunge <gyishun@gmail.com>
2016-09-06 11:12:11 -0700

    Bruteforce-recursive solution

--- minion_boardgame-unverified.java
+++ minion_boardgame-unverified.java
@@ -23,20 +23,72 @@ public class Answer {
         return totalMove;
     }
 
-    public static void BruteCount(int moveNum, int state, int dest) {
-        System.out.println(moveNum + " " + state + " " + dest);
-        if (dest == state) {
-            totalMove++;
-            return;
+import java.util.ArrayList;
+
+public class Answer {
+    public static int totalMove;
+    public static int answer(int t, int n) {
+
+        // Your code goes here.
+        if (t < n-1) {
+            return 0;
         }
-        if (moveNum == 0 || state < 0 || state > dest || moveNum < state-dest) {
-            System.out.println("Busted!");
-            return;
+
+        if (t == n-1) {
+            return 1;
         }
-        BruteCount(moveNum-1, state, dest);
-        BruteCount(moveNum-1, state-1, dest);
-        BruteCount(moveNum-1, state+1, dest);
+
+        int extraMove = t - n;
+        if (extraMove == 1) {
+            return t;
+        }
+        // let L = -1, S = 0, R = 1
+        totalMove = 0;
+        ArrayList<String> move = new ArrayList<>();
+        move.add("stay");
+        BruteCount(t-1, 0, n-1, move);
+        move.remove(move.size()-1);
+        move.add("right");
+        BruteCount(t-1, 1, n-1, move);
+
+        return totalMove;
     }
+
+    public static void BruteCount(int moveNum, int state, int dest, ArrayList<String> move) {
+        //System.out.println(moveNum + " " + state + " " + dest);
+        if (dest == state) {
+            System.out.println(moveNum + " " + state + " " + dest);
+            System.out.println("Ding!!");
+            totalMove++;
+            System.out.println(move);
+            return;
+        }
+        if (moveNum == 0) {
+            //System.out.println("Busted!");
+
+            return;
+        }
+        if( !(moveNum-1 < state-dest)) {
+            if (!(state - 1 < 0)) {
+                move.add("left");
+                BruteCount(moveNum - 1, state - 1, dest, move);
+                move.remove(move.size()-1);
+            }
+            if (!(state + 1 > dest)) {
+                move.add("right");
+                BruteCount(moveNum - 1, state + 1, dest, move);
+                move.remove(move.size()-1);
+            }
+            move.add("stay");
+            BruteCount(moveNum - 1, state, dest, move);
+            move.remove(move.size()-1);
+        }
+    }
+
+    public static void main(String[] args){
+        System.out.println(answer(5, 3));
+    }
+}
 
     public static void main(String[] args){
         System.out.println(answer(5, 3));
